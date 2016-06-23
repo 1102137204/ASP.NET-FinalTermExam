cb19bd25e3caf3831cebaa6940d5d72ed274acef
RPC <RPC@DESKTOP-L5BOH5N>
2016-06-23 15:33:38 +0800

    Add project files.

---
+++ finalEmployee/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(finalEmployee.Startup))]
+namespace finalEmployee
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
